0a1762813f924feb1d088afd2723ad53f7a92c84
ozgurersoz <ozgurersoz@gmail.com>
2022-10-08 11:57:51 +0200

    update

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "BMPlayer",
+    products: [
+        .library(name: "BMPlayer", targets: ["BMPlayer"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.6.0"),
+        .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView", from: "5.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "BMPlayer",
+            dependencies: ["SnapKit", "NVActivityIndicatorView"],
+            path: "Sources"
+        )
+    ]
+)
--- Sources/BMPlayer/Default/BMPlayerManager.swift
+++ Sources/BMPlayer/Default/BMPlayerManager.swift
@@ -61,3 +61,4 @@ open class BMPlayerManager {
         }
     }
 }
+
