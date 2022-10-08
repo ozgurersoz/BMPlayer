f318ba99b2dcdc1951ea71401456744365e3f96a
ozgurersoz <ozgurersoz@gmail.com>
2022-10-08 12:17:06 +0200

    update

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "BMPlayer",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         .library(name: "BMPlayer", targets: ["BMPlayer"]),
     ],
